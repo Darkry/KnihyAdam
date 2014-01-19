690519ed63b1a7cef36882055bcb21ebb71a88cd
Darkry <darkry@dark-project.cz>
2014-01-19 18:40:30 +0100

    Add book form

--- app/model/KnihaRepository.php
+++ app/model/KnihaRepository.php
@@ -4,4 +4,8 @@ namespace 	Knihovna;
 class KnihaRepository extends Repository
 {
 	
+	public function addBook($nazev, $autor, $zanr, $vytisky) {
+		$this->getTable()->insert(array("nazev" => $nazev, "celkemVytisku" => $vytisky, "volnychVytisku" => $vytisky, "autor_id" => $autor, "zanr_id" => $zanr));
+	}
+
 }
--- app/presenters/BookPresenter.php
+++ app/presenters/BookPresenter.php
@@ -5,7 +5,56 @@ use \Nette\Utils\Html;
 class BookPresenter extends BasePresenter
 {
 
+	private $authorModel;
+	private $genreModel;
+	private $bookModel;
+
+	public function startup() {
+		parent::startup();
+		$this->authorModel = $this->getService("autorRepository");
+		$this->bookModel = $this->getService("knihaRepository");
+		$this->genreModel = $this->getService("zanrRepository");
+	}
+
 	public function renderDefault() {
 		
+	}
+
+	public function createComponentAddBookForm() {
+		$form = new Form();
+
+		$form->addText("name", "Název: ")->setRequired("Vyplňte, prosím, název knihy.");
+		$form->addText("number", "Počet kopií: ")->setRequired("Prosíme, vyplňte počet kopií knihy, které knihovna vlastní.")
+												 ->addRule(Form::FLOAT, "Hodnota počtu výtisků musí být číslo.")
+												 ->setType("number");
+
+		$authorList = $this->authorModel->getAllAuthors();
+		$authors = array();
+		foreach($authorList as $author) {
+			$authors[$author->id] = $author->jmeno . " " . $author->prijmeni;
+		}
+		$form->addSelect("author", "Autor: ", $authors)->setPrompt("Vyberte autora")->setRequired("Prosíme, vyberte autora knihy.");
+
+
+		$genreList = $this->genreModel->getAllGenres();
+		$genres = array();
+		foreach($genreList as $genre) {
+			$genres[$genre->id] = $genre->nazev;
+		}
+		$form->addSelect("genre", "Žánr:: ", $genres)->setPrompt("Vyberte žánr")->setRequired("Prosíme, vyberte žánr knihy.");
+
+		$form->addSubmit("submit", "Přidat knihu");
+
+		$form->onSuccess[] = callback($this, "addBookFormSubmitted");
+
+		return $form;
+	}
+
+	public function addBookFormSubmitted(Form $form) {
+
+		$val = $form->getValues();
+		$this->bookModel->addBook($val->name, $val->author, $val->genre, $val->number);
+		$this->flashMessage("Kniha byla úspěšně přidána.");
+		$this->redirect("this");
 	}
 }
